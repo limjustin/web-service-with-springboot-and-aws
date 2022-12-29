d1c9d997f99fcb1efdde92760dec3ef123be2ba6
limjustin <limjyjustin@gmail.com>
2022-12-29 17:33:43 +0900

    commit chapter03

--- src/main/java/spring/webservice/springboot/service/posts/PostsService.java
+++ src/main/java/spring/webservice/springboot/service/posts/PostsService.java
@@ -3,8 +3,11 @@ package spring.webservice.springboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import spring.webservice.springboot.domain.posts.Posts;
 import spring.webservice.springboot.domain.posts.PostsRepository;
+import spring.webservice.springboot.web.dto.PostsResponseDto;
 import spring.webservice.springboot.web.dto.PostsSaveRequestDto;
+import spring.webservice.springboot.web.dto.PostsUpdateRequestDto;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +18,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
